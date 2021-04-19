1259f8f201d6621a89fb152c4dd1a828f886b3ed
starkz09 <starkzcoolz@gmail.com>
2021-04-19 18:03:12 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,14 +2,14 @@
 #!/bin/bash
 isExistApp=pgrep httpd
 if [[ -n $isExistApp ]]; then
-systemctl stop apache2.service
+sudo systemctl stop apache2.service
 fi
 isExistApp=pgrep mysqld
 if [[ -n $isExistApp ]]; then
-systemctl stop mariadb-server.service
+sudo systemctl stop mariadb.service
 fi
 isExistApp=pgrep php-fpm
 if [[ -n $isExistApp ]]; then
-systemctl stop php-fpm.service
+sudo systemctl stop php-fpm.service
 
 fi
