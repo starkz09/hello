8e7650fc0fca5d9f77c30650a5ef524848f6d97a
starkz09 <starkzcoolz@gmail.com>
2021-04-19 11:30:22 +0530

    Update stop_server.sh

--- WordPress-master/scripts/stop_server.sh
+++ WordPress-master/scripts/stop_server.sh
@@ -1,15 +1,5 @@
 
 #!/bin/bash
-isExistApp=pgrep httpd
-if [[ -n $isExistApp ]]; then
 sudo systemctl stop httpd.service
-fi
-isExistApp=pgrep mysqld
-if [[ -n $isExistApp ]]; then
 sudo systemctl stop mariadb.service
-fi
-isExistApp=pgrep php-fpm
-if [[ -n $isExistApp ]]; then
 sudo systemctl stop php-fpm.service
-
-fi
